e822895d451c5198a8288eb9d685e5d6a8c209f7
Ismael Parra M. <ismaelparramoussaif@gmail.com>
2025-11-07 10:17:16 +0100

    first commit

--- src/App.java
+++ src/App.java
@@ -1,32 +1,39 @@
+
 public class App {
+
     public static void main(String[] args) throws Exception {
-        
-        System.out.print("Por favor, introduzca un número entero: ");
-        long numeroInt = Long.parseLong(System.console().readLine());
+        try {
+            System.out.print("Por favor, introduzca un número entero: ");
+            long numeroInt = Long.parseLong(System.console().readLine());
 
-        System.out.print("Introduzca la posición dentro del número: ");
-        long posicion = Long.parseLong(System.console().readLine());
+            System.out.print("Introduzca la posición dentro del número: ");
+            long posicion = Long.parseLong(System.console().readLine());
 
-        System.out.print("Introduzca el nuevo dígito: ");
-        long digito = Long.parseLong(System.console().readLine());
-        
-        long numeroOriginal = numeroInt;
+            System.out.print("Introduzca el nuevo dígito: ");
+            long digito = Long.parseLong(System.console().readLine());
 
-        int longitud = 0;
+            long numeroOriginal = numeroInt;
 
-        do { 
-            numeroOriginal /=10;
-            longitud++;
-        } while (numeroOriginal > 0);
+            int longitud = 0;
 
-        long Izquierda = numeroInt / (long)(Math.pow(10, longitud - posicion + 1));
-        Izquierda = Izquierda * 10 + digito;
+            do {
+                numeroOriginal /= 10;
+                longitud++;
+            } while (numeroOriginal > 0);
 
-        long Derecha = numeroInt % (long)(Math.pow(10, longitud - posicion));
+            long Izquierda = numeroInt / (long) (Math.pow(10, longitud - posicion + 1));
+            Izquierda = Izquierda * 10 + digito;
 
-        numeroOriginal = Izquierda * (long)(Math.pow(10, longitud - posicion)) + Derecha;
+            long Derecha = numeroInt % (long) (Math.pow(10, longitud - posicion));
 
-        System.out.print("El número resultante es " + numeroOriginal);
+            numeroOriginal = Izquierda * (long) (Math.pow(10, longitud - posicion)) + Derecha;
+
+            System.out.print("El número resultante es " + numeroOriginal);
+        } catch (NumberFormatException e) {
+            System.out.println("Número mal introducido.");
+        } catch (Exception e) {
+            System.out.println("Ha ocurrido un error inesperado.");
+        }
     }
-    }
 
+}
